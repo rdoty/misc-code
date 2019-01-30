82c6278a422c13e2c1edd63d0d4af8088668644c
Rick Doty <rick.a.doty@gmail.com>
2019-01-30 09:35:55 -0800

    Update comments.

--- codingtests/ttacad.py
+++ codingtests/ttacad.py
@@ -2,6 +2,20 @@ import math
 import csv
 
 
+def get_data():
+    # Perhaps we'd store the data in a DB with these fields?
+    # 'distance','animal','height','weight','guess'
+    with open('ahw.csv', 'r') as data_file:
+        has_header = csv.Sniffer().has_header(data_file.read(1024))
+        data_file.seek(0)  # Rewind
+        reader = csv.reader(data_file)
+        if has_header:  # Skip header row
+            next(reader)
+        ret_val = list(reader)
+
+    return ret_val
+
+
 def guts():
     # basic flow (pseudo code)
     # prompt user for data height, weight
@@ -13,27 +27,18 @@ def guts():
     # prompt user to confirm guess == preference
     # record data height, weight, preference, guess
 
+    num_points_to_compare = 5
     h = float(input('Enter height: '))
     w = float(input('Enter weight: '))
 
-    # Perhaps we'd store the data in a DB with these fields?
-    # 'distance','animal','height','weight','guess'
-    with open('ahw.csv', 'r') as data_file:
-        has_header = csv.Sniffer().has_header(data_file.read(1024))
-        data_file.seek(0)  # Rewind
-        reader = csv.reader(data_file)
-        if has_header:  # Skip header row
-            next(reader)
-        csv_list = list(reader)
-
-    for row in csv_list:
+    csv_list = get_data()
+    for row in csv_list:  # calculate distances
         row.insert(0, math.sqrt((h - float(row[1])) ** 2 + (w - float(row[2])) ** 2))
-    csv_list.sort()
-    closest_animals = csv_list[:5]  # The first five are the closest to the given h, w values
-    closest_animals = [x[1] for x in closest_animals]
-    print closest_animals
+    csv_list.sort()  # find closest values
+    closest_animals = csv_list[:num_points_to_compare]
+    closest_animals = [x[1] for x in closest_animals]  # Get the animal preferences
 
-    the_guess = max(set(closest_animals), key=closest_animals.count)
+    the_guess = max(set(closest_animals), key=closest_animals.count)  # Get most common value
     the_prompt = "Is {:s} your preference? y/N ".format(the_guess)
     correct_guess = raw_input(the_prompt)
 
@@ -41,10 +46,7 @@ def guts():
     if correct_guess == 'y':
         new_row = [the_guess, h, w]
     else:
-        if the_guess == 'Cat':
-            new_row = ['Dog', h, w]
-        else:
-            new_row = ['Cat', h, w]
+        new_row = ['Dog', h, w] if the_guess == 'Cat' else ['Cat', h, w]
 
     with open('ahw.csv', 'ab') as data_file:
         writer = csv.writer(data_file)
