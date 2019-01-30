8f55deff5a6654d2ec6201f014b00f7aae9f5bda
Rick Doty <rick.a.doty@gmail.com>
2019-01-30 10:38:12 -0800

    Add check for data already in data set. Separate input gathering into separate method.

--- codingtests/ttacad.py
+++ codingtests/ttacad.py
@@ -16,15 +16,22 @@ def get_data():
     return ret_val
 
 
-def get_guess(h, w):
+def get_guess(the_input):
     data_list = get_data()
     num_points_to_compare = 5
     for row in data_list:  # calculate distances
-        row.insert(0, math.sqrt((h - float(row[1])) ** 2 + (w - float(row[2])) ** 2))
+        row.insert(0, math.sqrt((the_input[0] - float(row[1])) ** 2 + (the_input[1] - float(row[2])) ** 2))
     data_list.sort()  # find closest values
-    closest_animals = data_list[:num_points_to_compare]
-    closest_animals = [x[1] for x in closest_animals]  # Get the animal preferences
-    return max(set(closest_animals), key=closest_animals.count)  # Get most common value
+    closest_data = data_list[:num_points_to_compare]
+    is_guess = False if closest_data[0][0] == 0.0 else True  # we already have the value in the database
+    closest_animals = [x[1] for x in closest_data]  # Get the animal preferences
+    return [max(set(closest_animals), key=closest_animals.count), is_guess]  # Get most common value
+
+
+def get_input():
+    h = float(input('Enter height: '))
+    w = float(input('Enter weight: '))
+    return [h, w]
 
 
 def guts():
@@ -38,23 +45,26 @@ def guts():
     # prompt user to confirm guess == preference
     # record data height, weight, preference, guess
 
-    h = float(input('Enter height: '))
-    w = float(input('Enter weight: '))
+    h_w_input = get_input()
+    the_result = get_guess(h_w_input)
 
-    the_guess = get_guess(h, w)
+    if the_result[1]:  # If this is a guess - h/w not already in data
+        the_confirmation = "Is {:s} your preference? y/N ".format(the_result[0])
+        correct_guess = True if raw_input(the_confirmation) == 'y' else False
 
-    the_prompt = "Is {:s} your preference? y/N ".format(the_guess)
-    correct_guess = True if raw_input(the_prompt) == 'y' else False
+        # add new entry to csv file
+        if correct_guess:
+            new_row = [the_result[0], h_w_input[0], h_w_input[1]]
+        else:
+            new_row = ['Dog', h_w_input[0], h_w_input[1]] if the_result[0] == 'Cat'\
+                else ['Cat', h_w_input[0], h_w_input[1]]
 
-    # add new entry to csv file
-    if correct_guess:
-        new_row = [the_guess, h, w]
+        with open('ahw.csv', 'ab') as data_file:
+            writer = csv.writer(data_file)
+            writer.writerow(new_row)
     else:
-        new_row = ['Dog', h, w] if the_guess == 'Cat' else ['Cat', h, w]
-
-    with open('ahw.csv', 'ab') as data_file:
-        writer = csv.writer(data_file)
-        writer.writerow(new_row)
+        print "Found preference for Height: {:f}, Weight: {:f}. Preference: {:s}"\
+            .format(h_w_input[0], h_w_input[1], the_result[0])
 
 
 # Testing stub
