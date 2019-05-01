d04343e3a308d8a85809dd0c43c53e5e200fb5cf
Rick Doty <rick.a.doty@gmail.com>
2019-05-01 12:47:09 -0700

    Improved/simplified SMI exercise answer

--- codingtests/smallestmissinginteger.py
+++ codingtests/smallestmissinginteger.py
@@ -2,24 +2,48 @@
 # 2019.01.16 Here testing for TopTal
 
 
-def solution(a):
+def solution(the_list):
+    the_list.sort()  # Sort the array so we don't have to go to the end
+
     # Count up from one and check the array provided until we
     # don't find our value in the array
-
-    # Sort the array so we don't have to go to the end
-    a.sort()
-    ret_val = 1
+    missing_int = 1
     found = True
     while found:
-        for i in a:
-            if i == ret_val:
-                ret_val += 1
+        for entry in the_list:
+            if entry == missing_int:
+                missing_int += 1
         found = False
-    return ret_val
+    return missing_int
 
 # tests
 
 
+print("\nSolution 1")
 print(solution([1, 2, 4, 6, 3]))  # expects 5
 print(solution([1, 3, 5, 7, 9]))  # expects 2
 print(solution([-1, -2, -3, -5]))  # expects 1
+print(solution([1, 2, 3, 4, 5]))  # 6, expects ? -- this determines what to change above w.r.t. while loop
+
+
+# 2019.04.30 Revised answer
+
+
+def solution2(the_list):
+    the_list.sort()
+
+    missing_int = 0
+    for entry in the_list:
+        missing_int += 1
+        if entry != missing_int:
+            return missing_int
+    # return None  # be explicit? It does this anyway...
+
+
+# tests
+
+print("\nSolution 2")
+print(solution2([1, 2, 4, 6, 3]))  # expects 5
+print(solution2([1, 3, 5, 7, 9]))  # expects 2
+print(solution2([-1, -2, -3, -5]))  # expects 1
+print(solution2([1, 2, 3, 4, 5]))  # None, expects None
