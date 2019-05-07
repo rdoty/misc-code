e6cc73aaa3e03f4857f042a8ab35568249d7fb36
Rick Doty <rick.a.doty@gmail.com>
2019-05-07 15:58:02 -0700

    Some robustness improvements and error reporting.

--- codingtests/nc_exercise2.py
+++ codingtests/nc_exercise2.py
@@ -12,22 +12,27 @@ from bs4 import BeautifulSoup
 def solution():
     root_url = 'http://www.lolcats.com/'
     num_pages_to_scrape = 5  # number of pages to scrape from site
-    page_contents = urllib.request.urlopen(root_url)
-    soup = BeautifulSoup(page_contents, 'html.parser')
+    num_images_scraped = 0
+    soup = BeautifulSoup(urllib.request.urlopen(root_url), 'html.parser')
 
     while num_pages_to_scrape > 0:  # defined above
-        next_page = soup.find_all("a", text="next")
-        next_url = root_url + next_page[0]['href']
-
         images = soup.select(".lolcat")  # get the images on the current page
         for image in images:
             if image['src']:  # if we're able to parse the src url, write the image locally
                 urllib.request.urlretrieve(root_url + image['src'], os.path.basename(image['src']))
+                num_images_scraped += 1
 
         # We're done with all images on this page loaded, load the next page
-        page_contents = urllib.request.urlopen(next_url)
-        soup = BeautifulSoup(page_contents, 'html.parser')
-        num_pages_to_scrape -= 1  # decrement the count for pages to scrape
+        next_page = soup.find_all("a", text="next")
+        if next_page[0]['href']:  # Sanity check we have a valid next page
+            next_url = root_url + next_page[0]['href']
+            soup = BeautifulSoup(urllib.request.urlopen(next_url), 'html.parser')
+            num_pages_to_scrape -= 1  # decrement the count for pages to scrape
+        else:
+            print("Problem finding the next page to scrape. Exiting.")
+            num_pages_to_scrape = 0
+
+    print("Number of images scraped: {}".format(num_images_scraped))
 
 
 solution()
