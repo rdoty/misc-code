04793ae6a053befe52e7fc757f8636893e04154a
Rick Doty <rick.a.doty@gmail.com>
2026-07-13 15:28:48 +0200

    Improve implementation, tests
    - Handle cases where input lists are empty (could be cleaner?)
    - Clean up comments

--- codingtests/algorithms/sumOfZeroExists.java
+++ codingtests/algorithms/sumOfZeroExists.java
@@ -4,25 +4,26 @@
  * Goal: Determine if there is a single value from each set, that, when summed
  * with a single value of the other sets is zero
  *
- * false
- * [0, 1, 1, 1]
- * [0, 2, 2, 2]
- * [0, 3, 3, 3]
- * <p>
- * true
- * [1, 9, 9]
- * [9, -2, 9]
- * [9, 9, 1]
- * <p>
+ * should return false:
+ * [0, 1, 1, 1], [1, 2, 2, 2], [2, 3, 3, 3]
+ *
+ * should return true:
+ * [1, 9, 9], [9, -2, 9], [9, 9, 1]
+ *
  * Sum any one value from each set to get zero
  */
 class SumOfZeroExists {
 
-    private static boolean sumOfZeroExists(int[] a, int[] b, int[] c) {
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < b.length; j++) {
-                for (int k = 0; k < c.length; k++) {
-                    if (c[k] + b[j] + a[i] == 0) {
+    private static boolean sumOfZeroExists(int[] arrayA, int[] arrayB, int[] arrayC) {
+        // When an array is empty, use an array with a single zero value
+        int[] aNotEmpty = arrayA.length > 0 ? arrayA : new int[] { 0 };
+        int[] bNotEmpty = arrayB.length > 0 ? arrayB : new int[] { 0 };
+        int[] cNotEmpty = arrayC.length > 0 ? arrayC : new int[] { 0 };
+
+        for (int indexA = 0; indexA < aNotEmpty.length; indexA++) {
+            for (int indexB = 0; indexB < bNotEmpty.length; indexB++) {
+                for (int indexC = 0; indexC < cNotEmpty.length; indexC++) {
+                    if (cNotEmpty[indexC] + bNotEmpty[indexB] + aNotEmpty[indexA] == 0) {
                         return true;
                     }
                 }
@@ -36,15 +37,28 @@ class SumOfZeroExists {
     }
 
     private static void runTests() {
-        var firstExampleArray = new int[] { 1, 2, 4, 6, 3 };
-        var secondExampleArray = new int[] { -1 };
-        var thirdExampleArray = new int[] { 5 };
+        var demoPassingInput1 = new int[] { 1, 9, 9 };
+        var demoPassingInput2 = new int[] { 9, -2, 9 };
+        var demoPassingInput3 = new int[] { 9, 9, 1 };
+
+        var demoFailingInput1 = new int[] { 0, 1, 1, 1 };
+        var demoFailingInput2 = new int[] { 1, 2, 2, 2 };
+        var demoFailingInput3 = new int[] { 2, 3, 3, 3 };
+
+        var sequence1 = new int[] { 1, 2, 4, 6, 3 };
+        var negativeSequence1 = new int[] { -1, -2, -4, -6, -3 };
+        var singleElement = new int[] { 5 };
         var emptyArray = new int[] {};
 
-        System.out.println(sumOfZeroExists(firstExampleArray, emptyArray, emptyArray));
-        System.out.println(sumOfZeroExists(firstExampleArray, secondExampleArray, emptyArray));
-        System.out.println(sumOfZeroExists(firstExampleArray, secondExampleArray, thirdExampleArray));
-        System.out.println(sumOfZeroExists(firstExampleArray, thirdExampleArray, emptyArray));
+        System.out.println("Demo test results:");
+        System.out.println(sumOfZeroExists(demoPassingInput1, demoPassingInput2, demoPassingInput3));
+        System.out.println(sumOfZeroExists(demoFailingInput1, demoFailingInput2, demoFailingInput3));
+
+        System.out.println("\nOther test results:");
+        System.out.println(sumOfZeroExists(sequence1, emptyArray, emptyArray));
+        System.out.println(sumOfZeroExists(sequence1, negativeSequence1, emptyArray));
+        System.out.println(sumOfZeroExists(sequence1, negativeSequence1, singleElement));
+        System.out.println(sumOfZeroExists(sequence1, singleElement, emptyArray));
     }
 
 }
