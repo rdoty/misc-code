2286a4a0ce007b4965b676f1dbe83213094c9c8c
Rick Doty <rick.a.doty@gmail.com>
2026-07-14 20:59:40 +0200

    Update comments, add test data

--- codingtests/toptal/toptal_1.py
+++ codingtests/toptal/toptal_1.py
@@ -1,28 +1,27 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
-# Given two strings S and T consisting of N and M characters, respectively
-# determine whether string T can be obtained from string S by at most one
-# the operation from the following set on string S:
-
-# INSERT c -- T can be obtained from S by insertng a single character 'c'
-# REPLACE c d -- T can be obtained from S by replacing single occurence of 'c'
-# with a single character 'd' 
-# SWAP c d -- T can be obtained from S by swapping two adjanced characters
-# 'c' and 'd' (distint characters in the same order in both strings)
-# EQUAL -- No operations needed
-# IMPOSSIBLE -- None of the above works
+""" ORIGINAL INSTRUCTIONS
+    you can write to stdout for debugging purposes, e.g.
+    print("this is a debug message")
+    Given two strings S and T consisting of N and M characters, respectively
+    determine whether string T can be obtained from string S by at most one
+    the operation from the following set on string S:
+    # INSERT c -- T can be obtained from S by insertng a single character 'c'
+    # REPLACE c d -- T can be obtained from S by replacing single occurence of 'c'
+    # with a single character 'd' 
+    # SWAP c d -- T can be obtained from S by swapping two adjanced characters
+    # 'c' and 'd' (distint characters in the same order in both strings)
+    # EQUAL -- No operations needed
+    # IMPOSSIBLE -- None of the above works
+"""
 
 
 def which_allowed_method_obtains_t_from_s(s:str, t:str) -> str:
     # Determine difference(s) between s and t
-    # determine whether a single operation of insert, replace, swap are needed
-    # Easy first test on EQUAL
-    # String lengths off by more than one are IMPOSSIBLE
+    # determine whether a single operation of insert, replace, swap are possible
 
     if s == t:  # Are strings EQUAL
         return "EQUAL"
     elif abs(len(s) - len(t)) > 1:  # Are string off by more than a INSERT/SWAP?
-        return "IMPOSSIBLE"
+        return "IMPOSSIBLE"  # String lengths off by more than one are IMPOSSIBLE
     elif len(s) - len(t) == 1:  # T shorter by one character, can we INSERT?
         for index, item in enumerate(t):
             if ord(item) != ord(s[index]):
@@ -31,7 +30,7 @@ def which_allowed_method_obtains_t_from_s(s:str, t:str) -> str:
                     return "INSERT"
                 else:
                     return "IMPOSSIBLE"
-    elif len(t) - len(s) == 1:  # T shorter by one character, can we INSERT?
+    elif len(t) - len(s) == 1:  # T longer by one character, can we INSERT?
         for index, item in enumerate(s):
             if ord(item) != ord(t[index]):
                 test = (s[:index] + t[index] + s[index:])
@@ -79,6 +78,7 @@ def test_t_from_s_methods():
         {'source': 'nice', 'target': 'niece', 'is_t_in_s': True, 'method_contains': 'INSERT'},
         {'source': 'form', 'target': 'from', 'is_t_in_s': True, 'method_contains': 'SWAP'},
         {'source': 'o', 'target': 'odd', 'is_t_in_s': False, 'method_contains': 'IMPOSSIBLE'},
+        {'source': 'niece', 'target': 'nice', 'is_t_in_s': True, 'method_contains': 'INSERT'},
     ]
 
     print(f"\nTesting can_obtain_str_t_from_str_s():")
